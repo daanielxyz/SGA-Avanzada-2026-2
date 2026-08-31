75bee86e710ec46007d29ad42267a7a3195e0e00
daanielxyz <josed.valenciah@uqvirtual.edu.co>
2026-08-31 11:50:17 -0500

    default

---
+++ src/main/java/co/edu/uniquindio/sga_avanzada_2026_2/SgaAvanzada20262Application.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.sga_avanzada_2026_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaAvanzada20262Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgaAvanzada20262Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uniquindio/sga_avanzada_2026_2/SgaAvanzada20262ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.sga_avanzada_2026_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaAvanzada20262ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
